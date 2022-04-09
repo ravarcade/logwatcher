cda7de89d9ab0a860376e49ded93b6e35c7dfda0
Rafal Janicki <ravarcade@gmail.com>
2022-04-09 10:35:13 +0200

    Added logger and prepared for testing

--- src/filewatcher.ts
+++ src/filewatcher.ts
@@ -2,19 +2,24 @@
 
 import sanitize = require('sanitize-filename');
 import * as vscode from 'vscode';
+// import * as fs from 'fs';
+import { log } from './logger';
 
 // ikony: https://microsoft.github.io/vscode-codicons/dist/codicon.html
+// https://github.com/microsoft/vscode/issues/146182
 
 let watcher: vscode.FileSystemWatcher;
+let statusBarMsg: vscode.StatusBarItem;
 let files: string[];
 let exts: string[];
 let statusBars: vscode.StatusBarItem[];
-let statusBarMsg : vscode.StatusBarItem;
+let commands: vscode.Disposable[];
 
 // export async function activate(_context: vscode.ExtensionContext) {
 export async function activate(context: vscode.ExtensionContext) {
-    console.log("Start watcher");
+    log("Start watcher");
     setupWatcher(context);
+    setupConfigChange(context);
 }
 
 export async function deactivate() {
@@ -23,20 +28,47 @@ export async function deactivate() {
     }
 }
 
+// *** config change ***
+function setupConfigChange(context: vscode.ExtensionContext): void {
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
+        if (e.affectsConfiguration('conf.files.ext') || e.affectsConfiguration('conf.files.pattern')) {
+            vscode.window.showInformationMessage("logWatcher restart");
+            restartWatcher(context);
+        }
+    }));
+}
+
 // *** watcher ***
 function setupWatcher(context: vscode.ExtensionContext): void {
+    setupWatcherObjects(context);
+}
+
+function restartWatcher(context: vscode.ExtensionContext): void {
+    watcher.dispose();
+    statusBarMsg.dispose();
+
+    for (let i = 0; i < exts.length; ++i) {
+        statusBars[i].dispose();
+        commands[i].dispose();
+    }
+
+    setupWatcherObjects(context);
+}
+
+function setupWatcherObjects(context: vscode.ExtensionContext): void {
     let cfg = vscode.workspace.getConfiguration('', null);
     let ext = sanitize(cfg.get("conf.files.ext", "out,k3.txt"));
-    let pat = cfg.get("conf.files.pattern", "**/sctworkingdir/*/log/*-0");
+    let pat = cfg.get("conf.files.pattern", "**/sctworkingdir/*/logs/**/*");
     let fullWatchedPattern = `${pat}.{${ext}}`;
-    console.log("watching: " + fullWatchedPattern);
+    log("watching: " + fullWatchedPattern);
     exts = ext.split(',');
     files = new Array(exts.length);
     statusBars = new Array(exts.length);
+    commands = new Array(exts.length);
 
     exts.map(s => s.trim());
-    files.map(f => f = '');
     for (let i = 0; i < exts.length; ++i) {
+        files[i] = '';
         let ext = exts[i];
         statusBars[i] = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
         statusBars[i].text = `$(rocket) ${ext}`;
@@ -44,15 +76,17 @@ function setupWatcher(context: vscode.ExtensionContext): void {
         statusBars[i].hide();
 
         const cmdId = 'logwatcher.openLast_' + exts[i];
-        context.subscriptions.push(vscode.commands.registerCommand(cmdId, () => {
+        commands[i] = vscode.commands.registerCommand(cmdId, () => {
             openLog(i);
-        }));
+        });
+        context.subscriptions.push(commands[i]);
         statusBars[i].command = cmdId;
         context.subscriptions.push(statusBars[i]);
     }
 
     watcher = vscode.workspace.createFileSystemWatcher(fullWatchedPattern);
     watcher.onDidCreate(uri => {
+        log("create: ", uri.fsPath);
         let i = getSlot(uri.fsPath);
         if (i !== null) {
             files[i] = uri.fsPath;
@@ -61,12 +95,26 @@ function setupWatcher(context: vscode.ExtensionContext): void {
     });
 
     watcher.onDidDelete(uri => {
+        log("delete: ", uri.fsPath);
         let i = getSlot(uri.fsPath);
         if (i !== null) {
             files[i] = '';
             updateStatusBar(i);
         }
     });
+
+    watcher.onDidChange(uri => {
+        log("change: ", uri.fsPath);
+    });
+    // fs.watch("C:/Work/tmp/log-wacher-tests", async (eventType, filename) => {
+    //     log("\nThe file", filename, "was modified!");
+    //     log("The type of change was:", eventType);
+    //     // log.append("\nThe file")
+    //     // log.append(filename);
+    //     // log.appendLine("was modified!");
+    //     // log.append("The type of change was:");
+    //     // log.append(eventType);
+    //   });
 
     statusBarMsg = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
     statusBarMsg.text = `$(rocket) ${ext}`;
@@ -76,8 +124,7 @@ function setupWatcher(context: vscode.ExtensionContext): void {
 }
 
 function openLog(i: number) {
-    if (files.length > i && files[i].length > 0)
-    { 
+    if (files.length > i && files[i].length > 0) {
         openFile(files[i], 0);
     }
 }
@@ -117,7 +164,7 @@ function openFile(filename: string, line: number, column: number = 0) {
                 activeEditor.revealRange(newSelection);
             }
         });
-    }, (error: any) => {
+    }, (_error: any) => {
         // console.error(error);
         // debugger;
     });
---
+++ src/logger.ts
@@ -0,0 +1,40 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+export let logChannel = vscode.window.createOutputChannel("logwatcher");
+export function log(...o: any) {
+
+    function mapObject(obj: any) {
+
+        switch (typeof obj) {
+            case 'undefined':
+                return 'undefined';
+
+            case 'string':
+                return obj;
+
+            case 'number':
+                return obj.toString;
+
+            case 'object':
+                let ret: string = '';
+                for (const [key, value] of Object.entries(obj)) {
+                    ret += (`${key}: ${value}\n`);
+                }
+                return ret;
+
+            default:
+                return obj; //function,symbol,boolean
+
+        }
+
+    }
+
+    o.map((args: any) => {
+        logChannel.append('' + mapObject(args));
+    });
+    logChannel.appendLine('');
+    logChannel.show();
+
+}
